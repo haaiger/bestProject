5835736af8ade903c75c0b10579f6f5b58f6b264
ss-siberia <ss.elbrus@yandex.ru>
2023-04-18 19:34:29 +0600

    +navbar

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,14 +1,12 @@
 const React = require("react");
 
 module.exports = function Navbar({ userSession }) {
-  console.log(userSession, "userSessionuserSessionuserSession");
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <a className="navbar-brand btn btn-primary" href="/">
           Главная
         </a>
-        <button className="logout btn btn-danger">logout</button>
         <a className="navbar-brand btn btn-primary" href="/products/new">
           Добавить новый элемент
         </a>
