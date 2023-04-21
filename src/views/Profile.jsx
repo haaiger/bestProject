24226d7545f10265aac477d757a41abefec19596
Anton Dryuchin <antondryuchin@gmail.com>
2023-04-21 12:40:02 +0400

    111

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -12,14 +12,7 @@ module.exports = function Profile({ userSession, user, favsFull, filters }) {
   // console.log("filters========>", filters);
   return (
     <Layout userSession={userSession} style={style}>
-<<<<<<< HEAD
       <div className="mainContainer" style={{ display: "flex" }}>
-=======
-      <div
-        className="mainContainer"
-        style={{ display: 'flex', height: '100vh' }}
-      >
->>>>>>> origin
         <div className="operations">
           <h3 className="msg"></h3>
           {user?.isAdmin ? (
@@ -81,16 +74,7 @@ module.exports = function Profile({ userSession, user, favsFull, filters }) {
                 </label>
                 <input name="price" type="number" placeholder="Цена" />
                 <input name="description" type="text" placeholder="Описание" />
-<<<<<<< HEAD
-                <input
-                  name="photo"
-                  type="file"
-                  placeholder="Фото"
-                  value="test.jpg"
-                />
-=======
                 <input name="photo" type="file" placeholder="Фото" multiple />
->>>>>>> origin
                 <input name="address" type="text" placeholder="Адрес" />
                 <input name="geoTag" type="text" placeholder="Координаты" />
                 <button className="newAdBtn">Применить</button>
@@ -185,13 +169,8 @@ module.exports = function Profile({ userSession, user, favsFull, filters }) {
           </div>
           <div>Телефон: {user.phone}</div>
           <div>email: {user.email}</div>
-<<<<<<< HEAD
           <button className="editUserBtn">Изменить данные</button>
           <div class="editPassDiv">
-=======
-          <button className="editUserBtn">edit userInfo</button>
-          <div className="editPassDiv">
->>>>>>> origin
             <button className="editPassBtn">Изменить пароль</button>
           </div>
           <h4 className="passMsg"></h4>
